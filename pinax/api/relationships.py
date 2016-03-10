e3e2f0b90c2632113fd5623c5759ef6a78c80323
Brian Rosner <brian@brosner.com>
2016-03-09 17:00:23 -0700

    Set attr on Relationship

--- pinax/api/relationships.py
+++ pinax/api/relationships.py
@@ -8,6 +8,7 @@ class Relationship(object):
     def __init__(self, api_type, collection=False):
         self.api_type = api_type
         self.collection = collection
+        self.attr = None
 
     def resource_class(self):
         return registry.get(self.api_type)
