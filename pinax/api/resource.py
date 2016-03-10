946a09f2c3529a0b0ccdd9f5fe6b52385e27e771
Brian Rosner <brian@brosner.com>
2016-03-09 17:02:12 -0700

    Fixed value resolution for serialization

--- pinax/api/resource.py
+++ pinax/api/resource.py
@@ -163,7 +163,7 @@ class Resource(object):
         for attr in self.attributes:
             if isinstance(attr, str):
                 attr = Attribute(name=attr)
-            attributes[attr.name] = getattr(self.obj, attr.obj_attr)
+            attributes[attr.name] = resolve_value(getattr(self.obj, attr.obj_attr))
         relationships = {}
         for name, rel in self.relationships.items():
             rel_obj = relationships.setdefault(name, {
@@ -206,3 +206,13 @@ def resolve_include(resource, path, included):
         if rest:
             resolve_include(r, rest, included)
         included.add(r)
+
+
+def resolve_value(value):
+    if callable(value):
+        value = resolve_value(value())
+    if isinstance(value, datetime.datetime):
+        return rfc3339.encode(value)
+    if hasattr(value, "as_json"):
+        value = value.as_json()
+    return value
