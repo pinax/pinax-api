d09f87bac03aa7a6bc023ce480834efe735b2b47
Ethan McCreadie <ethanmcc@gmail.com>
2016-07-21 14:40:59 -0400

    Resolve datetime.date (#21)
    Thanks @ethanmcc, looks good!

--- pinax/api/resource.py
+++ pinax/api/resource.py
@@ -298,8 +298,10 @@ def resolve_include(resource, path, included):
 def resolve_value(value):
     if callable(value):
         value = resolve_value(value())
-    if isinstance(value, datetime.datetime):
-        return rfc3339.encode(value)
-    if hasattr(value, "as_json"):
+    elif isinstance(value, datetime.datetime):
+        value = rfc3339.encode(value)
+    elif isinstance(value, datetime.date):
+        value = datetime.date.isoformat(value)
+    elif hasattr(value, "as_json"):
         value = value.as_json()
     return value
